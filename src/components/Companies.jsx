78ac86538fd60382c7e04d4c209912312b8968ff
Guilherme Morgado <omorgadopt@gmail.com>
2022-11-30 13:34:22 +0100

    Proyecto Finalizado 😀

--- src/components/Companies.jsx
+++ src/components/Companies.jsx
@@ -22,52 +22,52 @@ const Companies = () => {
 				Findtrend make +1000 Startup grow
 			</h1>
 			<div className='w-full mt-16 grid md:grid-cols-4 grid-cols-2 gap-x-6 justify-center gap-y-6'>
-				<div className='h-[120px] rounded-xl bg-[#F5F5F5] flex justify-center items-center'>
+				<div className='px-5 h-[120px] rounded-xl bg-[#F5F5F5] flex justify-center items-center'>
 					<img src={Microsoft} alt='LogoMicrosoft' />
 				</div>
-				<div className='h-[120px] bg-[#F5F5F5] rounded-xl flex justify-center items-center'>
+				<div className='px-5 h-[120px] bg-[#F5F5F5] rounded-xl flex justify-center items-center'>
 					<img src={Allianz} alt='LogoAllianz' />
 				</div>
-				<div className='h-[120px] bg-[#F5F5F5] rounded-xl flex justify-center items-center'>
+				<div className='px-5 h-[120px] bg-[#F5F5F5] rounded-xl flex justify-center items-center'>
 					<img src={upWork} alt='LogoUpWorkd' />
 				</div>
-				<div className='h-[120px] bg-[#F5F5F5] rounded-xl flex justify-center items-center'>
+				<div className='px-5 h-[120px] bg-[#F5F5F5] rounded-xl flex justify-center items-center'>
 					<img src={Volvo} alt='LogoVolvo' />
 				</div>
-				<div className='h-[120px] bg-[#F5F5F5] rounded-xl flex justify-center items-center'>
+				<div className='px-5 h-[120px] bg-[#F5F5F5] rounded-xl flex justify-center items-center'>
 					<img src={Grab} alt='LogoGrab' />
 				</div>
-				<div className='h-[120px] bg-[#F5F5F5] rounded-xl flex justify-center items-center'>
+				<div className='px-5 h-[120px] bg-[#F5F5F5] rounded-xl flex justify-center items-center'>
 					<img src={Verizon} alt='LogoVerizon' />
 				</div>
-				<div className='h-[120px] bg-[#F5F5F5] rounded-xl flex justify-center items-center'>
+				<div className='px-5 h-[120px] bg-[#F5F5F5] rounded-xl flex justify-center items-center'>
 					<img src={Coinbase} alt='LogoCoinbase' />
 				</div>
-				<div className='h-[120px] bg-[#F5F5F5] rounded-xl flex justify-center items-center'>
+				<div className='px-5 h-[120px] bg-[#F5F5F5] rounded-xl flex justify-center items-center'>
 					<img src={Expedia} alt='LogoExpedia' />
 				</div>
-				<div className='h-[120px] bg-[#F5F5F5] rounded-xl flex justify-center items-center'>
+				<div className='px-5 h-[120px] bg-[#F5F5F5] rounded-xl flex justify-center items-center'>
 					<img src={Airtable} alt='LogoAirtable' />
 				</div>
-				<div className='h-[120px] bg-[#F5F5F5] rounded-xl flex justify-center items-center'>
+				<div className='px-5 h-[120px] bg-[#F5F5F5] rounded-xl flex justify-center items-center'>
 					<img src={Magento} alt='LogoMagento' />
 				</div>
-				<div className='h-[120px] bg-[#F5F5F5] rounded-xl flex justify-center items-center'>
+				<div className='px-5 h-[120px] bg-[#F5F5F5] rounded-xl flex justify-center items-center'>
 					<img src={Carrera} alt='LogoCarrera' />
 				</div>
-				<div className='h-[120px] bg-[#F5F5F5] rounded-xl flex justify-center items-center'>
+				<div className='px-5 h-[120px] bg-[#F5F5F5] rounded-xl flex justify-center items-center'>
 					<img src={Bonaza} alt='LogoBonaza' />
 				</div>
-				<div className='h-[120px] bg-[#F5F5F5] rounded-xl flex justify-center items-center'>
+				<div className='px-5 h-[120px] bg-[#F5F5F5] rounded-xl flex justify-center items-center'>
 					<img src={WeTransfer} alt='LogoWetransfer' />
 				</div>
-				<div className='h-[120px] bg-[#F5F5F5] rounded-xl flex justify-center items-center'>
+				<div className='px-5 h-[120px] bg-[#F5F5F5] rounded-xl flex justify-center items-center'>
 					<img src={Suzuki} alt='LogoSuzuki' />
 				</div>
-				<div className='h-[120px] bg-[#F5F5F5] rounded-xl flex justify-center items-center'>
+				<div className='px-5 h-[120px] bg-[#F5F5F5] rounded-xl flex justify-center items-center'>
 					<img src={Algolia} alt='LogoAlgolia' />
 				</div>
-				<div className='h-[120px] bg-[#F5F5F5] rounded-xl flex justify-center items-center'>
+				<div className='px-5 h-[120px] bg-[#F5F5F5] rounded-xl flex justify-center items-center'>
 					<img src={Heineken} alt='LogoHeineken' />
 				</div>
 			</div>
